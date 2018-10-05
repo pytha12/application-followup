7af6132f782ac0d9ff2ad8c9bb60fe761b6b8b82
Norbert Kofi-amafu <n.amafu@keyframedigital.com>
2018-10-04 20:35:39 -0400

    Follow up interview questions solution

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
 class Interview
-{
-	public $title = 'Interview test';
+{	
+    /* non static properties of a class cannot be called statically. 
+	   To solve the problem, precede $title with the keyword static. */
+	public static $title = 'Interview test';
+	//public $title = 'Interview test';
 }
 
 $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
@@ -15,7 +18,11 @@ $people = array(
 	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
 
-$person = $_POST['person'];
+/* Good to check if the 'person' array is set first. And set a default value in case it is not set. 
+   Secondly, the form method is GET (or get) and not POST hence one cannot retrieve the parameters from the $_POST array. 
+   We use the $_GET array instead. */
+$person = isset($_GET['person']) ? $_GET['person'] : '';
+//$person = $_POST['person'];
 
 ?>
 
@@ -33,15 +40,24 @@ $person = $_POST['person'];
 	<h1><?=Interview::$title;?></h1>
 
 	<?php
+	
 	// Print 10 times
+	/* The first condition for the 'for loop' should be the smaller value, namely the 'start value', middle condition is the 'end value'
+	   And the last condition is the 'increment' or 'decrement' value.
+	   Secondly php uses dot(.) for concatenation and not plus(+). PHP tries to add the strings by first casting them to integers(0) which is not the expected outcome. */
+	 
+	for ($i=0; $i<10; $i++) {
+		echo '<p>'.$lipsum.'</p>';
+	}
+	
+	/*
 	for ($i=10; $i<0; $i++) {
 		echo '<p>'+$lipsum+'</p>';
 	}
+	*/
 	?>
 
-
 	<hr>
-
 
 	<form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
 		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
@@ -67,13 +83,26 @@ $person = $_POST['person'];
 			</tr>
 		</thead>
 		<tbody>
+			<!--
+				$person variable is of type 'Array' (Associative Array) and not an instance of a class.
+				Therefore it is incorrect to access array values using the arrow sign.
+				Array values can be accessed using the angle bracket.
+			-->
 			<?php foreach ($people as $person): ?>
 				<tr>
-					<td><?=$person->first_name;?></td>
-					<td><?=$person->last_name;?></td>
-					<td><?=$person->email;?></td>
+					<td><?=$person["first_name"];?></td>
+					<td><?=$person["last_name"];?></td>
+					<td><?=$person["email"];?></td>
 				</tr>
 			<?php endforeach; ?>
+			
+			<?php //foreach ($people as $person): ?>
+				<tr>
+					<td><?=""; //$person->first_name;?></td>
+					<td><?=""; //$person->last_name;?></td>
+					<td><?=""; //$person->email;?></td>
+				</tr>
+			<?php //endforeach; ?>
 		</tbody>
 	</table>
 
